cbb1b6ed4a31330e110f41ad136baf7c7a84ffe9
sabilillah <sabilillah272@gmail.com>
2022-03-03 20:17:45 +0700

    add asset files

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,3 +1,45 @@
-// Export images
+// import assets
 
-export {};
+// icons
+import ActivityIcon from "./icons/activity-icon.svg";
+import ArrowDownIcon from "./icons/arrow-down-icon.svg";
+import ChatIcon from "./icons/chat-icon.svg";
+import CollectionIcon from "./icons/collection-icon.svg";
+import CrossIcon from "./icons/cross-icon.svg";
+import DashboardIcon from "./icons/dashboard-icon.svg";
+import ETHIcon from "./icons/ethereum-icon.svg";
+import ETHLogo from "./icons/ethereum-logo.svg";
+import HeartIcon from "./icons/heart-icon.svg";
+import MenuIcon from "./icons/menu-icon.svg";
+import NotifIcon from "./icons/notification-icon.svg";
+import SearchIcon from "./icons/search-icon.svg";
+import SettingIcon from "./icons/setting-icon.svg";
+import TransactionIcon from "./icons/transaction-icon.svg";
+
+// images
+import CardImg0 from "./card-images/card-img-0.png";
+import CardImg1 from "./card-images/card-img-1.png";
+
+// banners
+import DashboardBanner from "./banners/dashboard-header-banner.png";
+
+// Export all assets
+export {
+  ActivityIcon,
+  ArrowDownIcon,
+  ChatIcon,
+  CollectionIcon,
+  CrossIcon,
+  DashboardIcon,
+  ETHIcon,
+  ETHLogo,
+  HeartIcon,
+  MenuIcon,
+  NotifIcon,
+  SearchIcon,
+  SettingIcon,
+  TransactionIcon,
+  CardImg0,
+  CardImg1,
+  DashboardBanner,
+};
