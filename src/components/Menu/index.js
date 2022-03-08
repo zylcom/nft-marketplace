04a4a7e3b905c6924273b50b4fa99b0a72c8596b
Sabilillah <sabilillah272@gmail.com>
2022-03-08 14:47:31 +0700

    update menu component

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,9 +1,21 @@
 import React from "react";
+import {
+  ActivityIcon,
+  CollectionIcon,
+  DashboardIcon,
+  SettingIcon,
+  TransactionIcon,
+} from "src/assets";
+import { MenuItem } from "src/components";
 
 function Menu() {
   return (
-    <div>
-      <h1>Menu</h1>
+    <div className="flex flex-col py-6">
+      <MenuItem img={DashboardIcon} text="Dashboard" />
+      <MenuItem img={ActivityIcon} text="Activity" />
+      <MenuItem img={TransactionIcon} text="Transactions" />
+      <MenuItem img={CollectionIcon} text="Collections" />
+      <MenuItem img={SettingIcon} text="Settings" />
     </div>
   );
 }
