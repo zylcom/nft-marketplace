16dedbc2c0e111ea7709cad042c97bd5eb8cb619
Sabilillah <sabilillah272@gmail.com>
2022-03-09 10:21:22 +0700

    add navbar component to app component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import { Navbar } from "./components";
 
 function App() {
   return (
-    <div>
+    <div className="bg-dark-gun-metal">
+      <Navbar />
       <h1>Welcome </h1>
       <Outlet />
     </div>
