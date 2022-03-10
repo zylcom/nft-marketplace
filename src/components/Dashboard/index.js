8b758cdb2f52c6b72c418e6a847484d8c2cc5f2a
Sabilillah <sabilillah272@gmail.com>
2022-03-10 14:30:41 +0700

    update dashboard component

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -3,8 +3,10 @@ import { Header } from "src/components";
 
 function Dashboard() {
   return (
-    <div>
-      <Header />
+    <div className="gap-5 gap-y-7 p-4 md:grid md:grid-cols-12">
+      <div className="md:col-span7 xl:col-span-8">
+        <Header />
+      </div>
       <h1>Dashboard</h1>
     </div>
   );
