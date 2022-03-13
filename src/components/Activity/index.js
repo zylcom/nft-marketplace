6a41cf2440fe990cdc415920970db4fe7f33f7ce
sabilillah <sabilillah272@gmail.com>
2022-03-13 19:06:37 +0700

    feat(component): create activity component

---
+++ src/components/Activity/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { User } from "src/components";
+
+function Activity() {
+  return (
+    <div className="rounded-xl bg-white/5 p-3">
+      <User />
+    </div>
+  );
+}
+
+export default Activity;
