913358d8e91cb502d6a486c9d555f4dd03d45a8d
sabilillah <sabilillah272@gmail.com>
2022-03-13 18:39:56 +0700

    update top artist component

--- src/components/TopArtist/index.js
+++ src/components/TopArtist/index.js
@@ -3,13 +3,60 @@ import { User } from "src/components";
 
 function TopArtist() {
   return (
-    <div className="flex items-center">
-      <span className="mr-2 text-xl font-semibold text-white">1.</span>{" "}
-      <div className="flex w-full items-center justify-between">
-        <User />
-        <button className="my-[6.5px] rounded-lg bg-gradient-to-br from-lavender-indigo to-very-light-blue px-2.5 py-1.5">
-          <p className="my-1 text-xs text-white">Follow</p>
-        </button>
+    <div className="flex max-h-80 flex-col gap-y-5 overflow-y-auto">
+      <div className="flex items-center">
+        <span className="mr-2 text-xl font-semibold text-white">1.</span>{" "}
+        <div className="flex w-full items-center justify-between">
+          <User />
+          <button className="my-[6.5px] rounded-lg bg-gradient-to-br from-lavender-indigo to-very-light-blue px-2.5 py-1.5">
+            <p className="my-1 text-xs text-white">Follow</p>
+          </button>
+        </div>
+      </div>
+      <div className="flex items-center">
+        <span className="mr-2 text-xl font-semibold text-white">1.</span>{" "}
+        <div className="flex w-full items-center justify-between">
+          <User />
+          <button className="my-[6.5px] rounded-lg bg-gradient-to-br from-lavender-indigo to-very-light-blue px-2.5 py-1.5">
+            <p className="my-1 text-xs text-white">Follow</p>
+          </button>
+        </div>
+      </div>
+      <div className="flex items-center">
+        <span className="mr-2 text-xl font-semibold text-white">1.</span>{" "}
+        <div className="flex w-full items-center justify-between">
+          <User />
+          <button className="my-[6.5px] rounded-lg bg-gradient-to-br from-lavender-indigo to-very-light-blue px-2.5 py-1.5">
+            <p className="my-1 text-xs text-white">Follow</p>
+          </button>
+        </div>
+      </div>
+      <div className="flex items-center">
+        <span className="mr-2 text-xl font-semibold text-white">1.</span>{" "}
+        <div className="flex w-full items-center justify-between">
+          <User />
+          <button className="my-[6.5px] rounded-lg bg-gradient-to-br from-lavender-indigo to-very-light-blue px-2.5 py-1.5">
+            <p className="my-1 text-xs text-white">Follow</p>
+          </button>
+        </div>
+      </div>
+      <div className="flex items-center">
+        <span className="mr-2 text-xl font-semibold text-white">1.</span>{" "}
+        <div className="flex w-full items-center justify-between">
+          <User />
+          <button className="my-[6.5px] rounded-lg bg-gradient-to-br from-lavender-indigo to-very-light-blue px-2.5 py-1.5">
+            <p className="my-1 text-xs text-white">Follow</p>
+          </button>
+        </div>
+      </div>
+      <div className="flex items-center">
+        <span className="mr-2 text-xl font-semibold text-white">1.</span>{" "}
+        <div className="flex w-full items-center justify-between">
+          <User />
+          <button className="my-[6.5px] rounded-lg bg-gradient-to-br from-lavender-indigo to-very-light-blue px-2.5 py-1.5">
+            <p className="my-1 text-xs text-white">Follow</p>
+          </button>
+        </div>
       </div>
     </div>
   );
