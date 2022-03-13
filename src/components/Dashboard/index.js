08c81765ef43b7708f5a1f679f95e7df2f2b2d24
sabilillah <sabilillah272@gmail.com>
2022-03-13 19:36:41 +0700

    fix(dashboard-component): fix className typo

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -4,7 +4,7 @@ import { Header, Category, Card, TopArtist, Activity } from "src/components";
 function Dashboard() {
   return (
     <div className="gap-5 gap-y-7 md:grid md:grid-cols-12">
-      <div className="md:col-span7 xl:col-span-8">
+      <div className="md:col-span-7 xl:col-span-8">
         <Header />
       </div>
       <div className="mt-6 md:order-3 md:col-span-7 md:mt-0 xl:col-span-8">
