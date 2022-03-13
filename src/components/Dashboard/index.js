100f7f7303ba54a946b60ee1e8ed8bac57e83564
sabilillah <sabilillah272@gmail.com>
2022-03-13 13:24:47 +0700

    update(dashboard-component): add card component

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Header, Category } from "src/components";
+import { Header, Category, Card } from "src/components";
 
 function Dashboard() {
   return (
@@ -10,6 +10,15 @@ function Dashboard() {
       <div className="mt-6 md:order-3 md:col-span-7 md:mt-0 xl:col-span-8">
         <h1 className="text-xl font-semibold text-white">Explore</h1>
         <Category />
+        <div className="outer-card-wrapper absolute mt-[127px] h-[90vw] w-[66vh] min-w-[400px] max-w-[410px] origin-top-left translate-y-[340px] -rotate-90 select-none overflow-y-auto md:relative md:mt-[83px] md:h-auto md:w-auto md:max-w-none md:transform-none">
+          <div className="flex w-auto origin-top-left translate-x-96 rotate-90 flex-row gap-3 md:grid md:transform-none md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+          </div>
+        </div>
       </div>
     </div>
   );
