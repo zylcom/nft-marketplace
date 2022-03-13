479a7a801f8547ee7f35d16dc9c6a436027e0dd1
sabilillah <sabilillah272@gmail.com>
2022-03-13 18:19:18 +0700

    update top artist component

--- src/components/TopArtist/index.js
+++ src/components/TopArtist/index.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { User } from "src/components";
 
 function TopArtist() {
   return (
-    <div>
-      <h1>TopArtist</h1>
+    <div className="flex items-center">
+      <span className="mr-2 text-xl font-semibold text-white">1.</span>{" "}
+      <div className="flex w-full items-center justify-between">
+        <User />
+        <button className="my-[6.5px] rounded-lg bg-gradient-to-br from-lavender-indigo to-very-light-blue px-2.5 py-1.5">
+          <p className="my-1 text-xs text-white">Follow</p>
+        </button>
+      </div>
     </div>
   );
 }
